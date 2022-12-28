f0aa04cdfb2986972f0be8a177aa0fbca9a402f7
jeremy <jeremyeparker@yahoo.com>
2022-12-28 16:48:59 -0500

    spring boot| web services

---
+++ src/main/java/com/example/O2auth/O2authApplication.java
@@ -0,0 +1,13 @@
+package com.example.O2auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2authApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2authApplication.class, args);
+	}
+
+}
